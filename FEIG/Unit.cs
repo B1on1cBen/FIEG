8a21496473f26242557c5b97ee3134d895cdba4b
Unknown <bengg2015@gmail.com>
2018-09-26 14:46:25 -0400

    Refactored Unit.ValidAlongX Again

--- FEIG/Unit.cs
+++ FEIG/Unit.cs
@@ -522,32 +522,47 @@ namespace FEIG
             Point dif = B - A;
             Point current = A;
             Point nextPoint = current;
+
+            bool canPass = true;
             int distanceMod = 0;
 
-            while (current.X != B.X)
+            while (canPass)
             {
-                nextPoint.X += Math.Sign(dif.X);
-                TileType tile = Level.GetTile(nextPoint).type;
+                // Make the next point's X increase, either positive or negative
+                if (dif.X > 0)
+                    nextPoint.X++;
+                else
+                    nextPoint.X--;
 
-                if (!CanPass(tile))
-                    return false;
+                TileType tile = Level.GetTile(nextPoint).type;
 
                 // Can't pass onto next point if the current point is a forest and we are infantry (infantry get slowed down by forests.)
                 if (moveType == MoveType.Infantry && tile == TileType.Forest)
                     distanceMod--;
 
+                canPass = CanPass(tile);
+
                 if (DistanceTo(nextPoint) > moveRanges[(int)moveType] + distanceMod)
-                    return false;
+                    canPass = false;
 
                 // Cannot pass through enemies
                 Unit unit = Game1.GetUnit(nextPoint);
                 if (unit != null && unit.team != team)
+                    canPass = false;
+
+                // Make the current point equal the next point if
+                // next point is valid.
+                if (canPass)
+                    current = nextPoint;
+                else
                     return false;
 
-                current = nextPoint;
+                // End with a true if we are at the goal
+                if (current.X == B.X)
+                    return true;
             }
 
-            return true;
+            return false;
         }
 
         private bool ValidAlongY(Point A, Point B)
